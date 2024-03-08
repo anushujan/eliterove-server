2e5fa9a6726fa02d07dd6cfbb736f297133c13ca
anushujan satkunarasa <43997407+anushujan@users.noreply.github.com>
2024-03-08 12:53:47 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,218 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors());
+// Dummy data for demonstration purposes
+const cardData = [
+  {
+    id: 1,
+    imageUrl: "https://pics.clipartpng.com/Black_Mercedes_Benz_S_Class_2012_Car_PNG_Clipart-106.png",
+    title: "Mercedes-Benz",
+    model:"AMG G 63",
+    seats:3,
+    luggage: 2,
+    hourlyrate:60,
+    dailyrate:60,
+    decription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con Duis aute irure dolor in reprehenderit esse cillum ",
+    details:[
+      "Lorem ipsum dolo",
+    ]
+    
+  },
+  {
+    id: 2,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Gizmo",
+    model:"AMG G 63",
+    seats:3,
+    luggage: 2,
+    hourlyrate:60,
+    dailyrate:60,
+    decription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con Duis aute irure dolor in reprehenderit esse cillum ",
+    details:[
+      "Lorem ipsum dolo",
+    ]
+    
+
+    
+  },
+  {
+    id: 3,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Cyclone",
+    model:"AMG G 63",
+    seats:3,
+    luggage: 2,
+    hourlyrate:60,
+    dailyrate:60,
+    decription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con Duis aute irure dolor in reprehenderit esse cillum ",
+    details:[
+      "Lorem ipsum dolo",
+    ]
+    
+   
+    
+  },
+  {
+    id: 4,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Mercedes-Benz",
+    model:"AMG G 63",
+    seats:3,
+    luggage: 2,
+    hourlyrate:60,
+    dailyrate:60,
+    decription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo con Duis aute irure dolor in reprehenderit esse cillum ",
+    details:[
+      "Lorem ipsum dolo",
+    ]
+    
+
+    
+  },
+  {
+    id: 5,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Mercedes-Benz",
+    model:"AMG G 63",
+    seats:3,
+    luggage: 2,
+    hourlyrate:60,
+    dailyrate:60,
+
+    
+  },
+  {
+    id: 6,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Mercedes-Benz",
+    model:"AMG G 63",
+    seats:3,
+    luggage: 2,
+    hourlyrate:60,
+    dailyrate:60,
+
+    
+  },
+  {
+    id: 7,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Mercedes-Benz",
+    model:"AMG G 63",
+    seats:3,
+    luggage: 2,
+    hourlyrate:60,
+    dailyrate:60,
+
+    
+  },
+  {
+    id: 8,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Mercedes-Benz",
+    model:"AMG G 63",
+    seats:3,
+    luggage: 2,
+    hourlyrate:60,
+    dailyrate:60,
+
+    
+  },
+
+  {
+    id: 9,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Mercedes-Benz",
+    model:"AMG G 63",
+    seats:3,
+    luggage: 2,
+    hourlyrate:60,
+    dailyrate:60,
+
+    
+  },
+
+  {
+    id: 10,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Mercedes-Benz",
+    model:"AMG G 63",
+    seats:3,
+    luggage: 2,
+    hourlyrate:60,
+    dailyrate:60,
+
+    
+  },
+];
+
+
+const ChauffeurData = [
+  {
+    id: 11,
+    imageUrl: "https://pics.clipartpng.com/Black_Mercedes_Benz_S_Class_2012_Car_PNG_Clipart-106.png",
+    title: "Rolls Royce Cullinan",
+    model:"Luxury SUV",
+    seats:3,
+    luggage: 2,
+   
+
+    
+  },
+  {
+    id: 12,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Rolls Royce Cullinan",
+    model:"Luxury SUV",
+    seats:3,
+    luggage: 2,
+    
+
+    
+  },
+  {
+    id: 13,
+    imageUrl: "https://images.app.goo.gl/V8AGvH71YMB5o5Kj9.png",
+    title: "Rolls Royce Cullinan",
+    model:"Luxury SUV",
+    seats:3,
+    luggage: 2,
+   
+
+    
+  },
+  
+];
+
+
+
+app.get("/api/auto-gear", (req, res) => {
+  res.json(ChauffeurData);
+});
+
+// Define routes
+app.get("/api/helmets", (req, res) => {
+  res.json(cardData);
+});
+
+app.get("/api/helmets/:id", (req, res) => {
+  const { id } = req.params;
+  const car = cardData.find((car) => car.id === parseInt(id));
+  if (car) {
+    res.json(car);
+  } else {
+    res.status(404).json({ message: "Car not found" });
+  }
+});
+
+
+
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
